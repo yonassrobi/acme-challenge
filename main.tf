d077e9b24859167d7cc16f444c31f3b468c968e3
Yonas <yonas@samtek-inc.com>
2022-08-05 16:30:05 -0400

    image tag update

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 
+
  module "ecs-fargate-service" {
   // depends_on                = [module.vpc]
    source                  = "./modules/ecs-fargate-service"
@@ -6,6 +7,7 @@
    environment             = var.environment
    project                 = var.project
    region                  = var.region
+   image_id                = var.image_id
    app_definitions         = local.app_definitions
    health_check_path       = "/swagger-ui/index.html"
  }
