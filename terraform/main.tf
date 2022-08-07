1cabc7a2bd96e20335e25df3bb92b071de81f3e4
Yonas <yonas@samtek-inc.com>
2022-08-07 13:52:33 -0400

    config updates

---
+++ terraform/main.tf
@@ -0,0 +1,15 @@
+
+
+ module "ecs-fargate-service" {
+  // depends_on                = [module.vpc]
+   source                  = "./modules/ecs-fargate-service"
+   vpc_id                  = var.vpc_id
+   environment             = var.environment
+   project                 = var.project
+   region                  = var.region
+   app_definitions         = local.app_definitions
+   health_check_path       = "/swagger-ui/index.html"
+ }
+ 
+
+ 
